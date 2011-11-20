e4e330618644286532c725a629e9ad99b70cfa52
andreas.gebauer.berlin@gmail.com <andreas.gebauer.berlin@gmail.com@8e80d15b-6d02-9f82-3623-a332166a4f9c>
2011-11-20 22:52:31 +0000

    added test for grabbing pixels on the screen

--- boblight4j-client-X11/src/test/java/org/boblight4j/client/X11/AbstractX11GrabberTest.java
+++ boblight4j-client-X11/src/test/java/org/boblight4j/client/X11/AbstractX11GrabberTest.java
@@ -3,6 +3,7 @@ package org.boblight4j.client.X11;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import gnu.x11.Display;
 import gnu.x11.Screen;
@@ -42,10 +43,16 @@ public class AbstractX11GrabberTest {
 
 	@Test
 	public void testGrabPixelAt() throws IOException, BoblightException {
+		int scrWidth = 1280;
+		int scrHeight = 720;
+		int size = 16;
 
 		Display display = mock(Display.class);
 		display.default_screen = mock(Screen.class);
+		display.default_screen.height = scrHeight;
+		display.default_screen.width = scrWidth;
 
+		Whitebox.setInternalState(testable, "size", size);
 		when(testable.getDisplay()).thenReturn(display);
 
 		// stop after first cycle
@@ -60,5 +67,18 @@ public class AbstractX11GrabberTest {
 
 		testable.run();
 
+		double cellWidth = (double) scrWidth / (double) size;
+		double cellHeight = (double) scrHeight / size;
+
+		for (int width = 0; width < size; width++)
+		{
+			for (int height = 0; height < size; height++)
+			{
+				verify(testable).grabPixelAt(
+						(int) (cellWidth / 2 + cellWidth * width),
+						(int) (scrHeight / size / 2 + cellHeight * height));
+				verify(client).addPixel(width, height, null);
+			}
+		}
 	}
 }
