68be8c544afbeae50675b8ac7315f3ba781b8f79
andreas.gebauer.berlin@gmail.com <andreas.gebauer.berlin@gmail.com@8e80d15b-6d02-9f82-3623-a332166a4f9c>
2012-04-25 22:36:50 +0000

    refactored light processing on the client side; fixed a lot of tests; dependency-plugin for xbmc addons is executed in phase package; fixed basedir problem with deploy script

--- boblight4j-server/boblight4j-server-xbmc-plugin/deploy.sh
+++ boblight4j-server/boblight4j-server-xbmc-plugin/deploy.sh
@@ -15,11 +15,13 @@ if [ -z $XBMC_ADDONS_DIR ]; then
 	exit 1
 fi
 
+BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
+
 REMOTE_ADDONS_DIR=$XBMC_USER@$XBMC_HOST:$XBMC_ADDONS_DIR
 
 HOST="openelec"
 ADDONS_PATH="/storage/.xbmc/addons"
 
-scp -r target/resources/* $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/
-scp target/dependency/boblight4j-*server*.jar $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/bin
-#scp -r target/dependency/boblight4j-*server*/lib $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/bin
+scp -r $BASEDIR/target/resources/* $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/
+scp $BASEDIR/target/dependency/boblight4j-*server*/*.jar $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/bin
+scp -r $BASEDIR/target/dependency/boblight4j-*server*/lib $REMOTE_ADDONS_DIR/service.multimedia.boblight4j-server/bin
